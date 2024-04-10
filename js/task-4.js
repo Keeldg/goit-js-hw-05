c6004be82f61b1cd756cd49c4b92055c30ef3a12
Keeldg <154369545+Keeldg@users.noreply.github.com>
2024-04-11 00:11:20 +0300

    hw5task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,7 @@
 const getTotalBalanceByGender = (users, gender) => {
-  return users.map(res);
+  return users
+    .filter((user) => user.gender === gender)
+    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
 };
 
 const clients = [
